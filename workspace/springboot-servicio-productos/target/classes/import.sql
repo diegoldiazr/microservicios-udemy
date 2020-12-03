f8670cb650bf0786e3166f41e24852496f8abd5c
Diaz Rabadan, Diego <diegoldiazr@gmail.com>
2020-12-03 15:15:23 +0100

    initial import 2

---
+++ workspace/springboot-servicio-productos/target/classes/import.sql
@@ -0,0 +1,8 @@
+insert into productos (nombre, precio, created_at) values ('Nike', 12.98, now());
+insert into productos (nombre, precio, created_at) values ('Reebok', 125.98, now());
+insert into productos (nombre, precio, created_at) values ('NewBalance', 14452.98, now());
+insert into productos (nombre, precio, created_at) values ('Sketchers', 12342.98, now());
+insert into productos (nombre, precio, created_at) values ('Snakes', 444.98, now());
+insert into productos (nombre, precio, created_at) values ('Apple', 24.98, now());
+insert into productos (nombre, precio, created_at) values ('Amazon', 1.98, now());
+insert into productos (nombre, precio, created_at) values ('El corte ingles', 442.98, now());
