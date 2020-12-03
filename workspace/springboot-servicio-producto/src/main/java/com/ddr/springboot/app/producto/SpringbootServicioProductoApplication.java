58733c2355475659516470ec4ffa2adf0810cf6b
Diaz Rabadan, Diego <ddiaz@eulen.com>
2020-12-03 16:40:41 +0100

    create product project

---
+++ workspace/springboot-servicio-producto/src/main/java/com/ddr/springboot/app/producto/SpringbootServicioProductoApplication.java
@@ -0,0 +1,13 @@
+package com.ddr.springboot.app.producto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioProductoApplication.class, args);
+	}
+
+}
---
+++ workspace/springboot-servicio-producto/src/test/java/com/ddr/springboot/app/producto/SpringbootServicioProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddr.springboot.app.producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
