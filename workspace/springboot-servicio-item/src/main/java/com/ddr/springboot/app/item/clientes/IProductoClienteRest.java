ffb1fd4adf1158750bdf4f41ea0cc9811edcda46
Diaz Rabadan, Diego <ddiaz@eulen.com>
2020-12-03 17:36:43 +0100

    Using Feign

---
+++ workspace/springboot-servicio-item/src/main/java/com/ddr/springboot/app/item/clientes/IProductoClienteRest.java
@@ -0,0 +1,20 @@
+package com.ddr.springboot.app.item.clientes;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.ddr.springboot.app.item.model.Producto;
+
+@FeignClient(name="servicio-productos", url="localhost:8001")
+public interface IProductoClienteRest {
+	
+	@GetMapping("/listar")
+	public List<Producto> listar();
+
+	@GetMapping("/ver/{id}")
+	public Producto detalle(@PathVariable Long id);
+
+}
--- workspace/springboot-servicio-item/src/main/java/com/ddr/springboot/app/item/controller/ItemController.java
+++ workspace/springboot-servicio-item/src/main/java/com/ddr/springboot/app/item/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.ddr.springboot.app.item.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import com.ddr.springboot.app.item.service.IItemService;
 public class ItemController {
 
 	@Autowired
+	@Qualifier("ItemServiceFeign")
 	private IItemService itemService;
 	
 	@GetMapping("/listar")
