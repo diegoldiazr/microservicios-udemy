3efb1989fe2a57b3ed7db32c1a97e1952a260c85
Diaz Rabadan, Diego <diegoldiazr@gmail.com>
2020-12-04 13:50:16 +0100

    incorporacion de eureka en los proyectos

--- workspace/springboot-servicio-item/src/main/java/com/ddr/springboot/app/item/SpringbootServicioItemApplication.java
+++ workspace/springboot-servicio-item/src/main/java/com/ddr/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,11 +2,11 @@ package com.ddr.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @EnableFeignClients
-@RibbonClient(name="servicio-productos")
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
 
