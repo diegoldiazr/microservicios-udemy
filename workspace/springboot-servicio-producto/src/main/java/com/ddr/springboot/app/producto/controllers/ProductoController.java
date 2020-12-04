b66bcdc3b716e0ba4d9c6d54e7b0d4ee524f167f
Diego Diaz <diegoldiazr@gmail.com>
2020-12-04 09:18:51 +0100

    Una forma mas sencilla de obtener el port desde la variable de entorno.
    Signed-off-by: Diego Diaz <diegoldiazr@gmail.com>

--- workspace/springboot-servicio-producto/src/main/java/com/ddr/springboot/app/producto/controllers/ProductoController.java
+++ workspace/springboot-servicio-producto/src/main/java/com/ddr/springboot/app/producto/controllers/ProductoController.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +15,10 @@ import com.ddr.springboot.app.producto.services.IProductoService;
 @RestController
 public class ProductoController {
 	
-	//para traer el port
-	@Autowired
-	private Environment env;
+	//traer el port de forma mas sencilla
+	@Value("${server.port}")
+	private Integer port;
+	
 
 	@Autowired
 	private IProductoService productoService;
@@ -25,7 +26,7 @@ public class ProductoController {
 	@GetMapping("/listar")
 	public List<Producto> findAll(){
 		return productoService.findAll().stream().map(producto -> {
-			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			producto.setPort(port);
 			return producto;
 			}).collect(Collectors.toList());
 		
@@ -34,7 +35,7 @@ public class ProductoController {
 	@GetMapping("/ver/{id}")
 	public Producto detalle(@PathVariable Long id) {
 		Producto producto = productoService.findById(id);
-		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		producto.setPort(port);
 		return producto;
 	}
 }
